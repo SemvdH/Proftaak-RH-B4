7df8d98ed9baa0f36643d6ff91514b78b3448764
Sem van der Hoeven <semmer99@gmail.com>
2020-09-18 17:08:19 +0200

    test

--- RH-Engine/Program.cs
+++ RH-Engine/Program.cs
@@ -18,7 +18,7 @@ namespace RH_Engine
         private static PC[] PCs = {
             //new PC("DESKTOP-M2CIH87", "Fabian"),
             new PC("T470S", "Shinichi"),
-            new PC("DESKTOP-DHS478C", "semme"),
+            //new PC("DESKTOP-DHS478C", "semme"),
             //new PC("DESKTOP-TV73FKO", "Wouter"),
             //new PC("DESKTOP-SINMKT1", "Ralf"),
             //new PC("NA", "Bart")
