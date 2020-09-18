a9036e6fc2e91fb3d6e8882476b04a7324116bc2
fabjuuuh <f.degroot1@student.avans.nl>
2020-09-18 10:50:17 +0200

    Terrrain command changed

--- RH-Engine/CreateGraphics.cs
+++ RH-Engine/CreateGraphics.cs
@@ -15,49 +15,19 @@ namespace RH_Engine
             this.tunnelID = tunnelID;
         }
 
-        public string TerrainCommand()
+        public string TerrainCommand(int[] sizeAray, int[] heightsArray)
         {
-            StringBuilder sb = new StringBuilder();
-            StringWriter sw = new StringWriter(sb);
-
-            using (JsonWriter writer = new JsonTextWriter(sw))
+            dynamic payload = new
             {
-                writer.Formatting = Formatting.Indented;
+                id = "scene/terrain/add",
+                data = new
+                {
+                    size = sizeAray,
+                    heights = heightsArray
+                }
 
-                writer.WriteStartObject();
-                writer.WritePropertyName("id");
-                writer.WriteValue("tunnel/send");
-                writer.WritePropertyName("data");
-                writer.WriteStartObject();
-                writer.WritePropertyName("dest");
-                writer.WriteValue(tunnelID);
-                writer.WritePropertyName("data");
-                writer.WriteStartObject();
-                writer.WritePropertyName("id");
-                writer.WriteValue("scene/terrain/add");
-                writer.WritePropertyName("data");
-                writer.WriteStartObject();
-                writer.WritePropertyName("size");
-                writer.WriteStartArray();
-                writer.WriteValue(2);
-                writer.WriteValue(2);
-                writer.WriteEndArray();
-                writer.WritePropertyName("heights");
-                writer.WriteStartArray();
-                writer.WriteValue(30000000);
-                writer.WriteValue(2);
-                writer.WriteValue(4);
-                writer.WriteValue(7);
-                writer.WriteEndArray();
-                writer.WriteEndObject();
-                writer.WriteEndObject();
-                writer.WriteEndObject();
-                writer.WriteEndObject();
-
-            }
-
-            Console.WriteLine(sb.ToString());
-            return sb.ToString();
+            };
+            return JsonConvert.SerializeObject(Payload(payload));
         }
 
         public string SkyboxCommand(double timeToSet)
