28242ff052755afb243e55e4da7b167113635239
Sem van der Hoeven <semmer99@gmail.com>
2020-09-23 11:50:25 +0200

    added message class and added references to server and client projects

---
+++ Message/Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Message
+{
+    public class Message
+    {
+        public string Identifier
+        {
+            get;set;
+        }
+
+        public string Payload
+        {
+            get;set;
+        }
+
+        public Message(string identifier, string payload)
+        {
+            this.Identifier = identifier;
+            this.Payload = payload;
+        }
+
+        public string Serialize()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+        public static Message Deserialize(string json)
+        {
+            return (Message)JsonConvert.DeserializeObject(json);
+        }
+    }
+}
