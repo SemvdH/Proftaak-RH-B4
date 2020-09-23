a52f1ea7ede510d279669194927eafefb3f05e69
shinichi <shinichi@hotmail.nl>
2020-09-23 11:12:10 +0200

    started client

---
+++ Client/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Client
+    {
+        private TcpClient client;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //connect fiets
+
+
+
+
+        }
+
+        public Client() : this("localhost", 5555)
+        {
+
+        }
+
+        public Client(string adress, int port)
+        {
+            this.client = new TcpClient();
+            client.BeginConnect(adress, 15243, new AsyncCallback(OnConnect), null);
+        }
+    }
+}
