91bb0d65b7a40175d4f3da3d5067b110b4a16a78
shinichi <shinichi@hotmail.nl>
2020-10-07 16:31:44 +0200

    added status

--- ClientApp/ViewModels/LoginViewModel.cs
+++ ClientApp/ViewModels/LoginViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Input;
 using ClientApp.Utils;
@@ -12,15 +13,15 @@ namespace ClientApp.ViewModels
     class LoginViewModel : ObservableObject
     {
         public string Username { get; set; }
-        //private MainWindowViewModel mainWindowViewModel;
+        private MainWindowViewModel mainWindowViewModel;
         public LoginViewModel(MainWindowViewModel mainWindowViewModel)
         {
-            //this.mainWindowViewModel = mainWindowViewModel;
+            this.mainWindowViewModel = mainWindowViewModel;
             LoginCommand = new RelayCommand<object>((parameter) =>
             {
                 Debug.WriteLine($"username {Username} password {((PasswordBox)parameter).Password}");
                 //TODO send username and password to server
-                mainWindowViewModel.SelectedViewModel = new MainViewModel();
+                this.mainWindowViewModel.SelectedViewModel = new MainViewModel();
             });
         }
 
--- ClientApp/ViewModels/MainViewModel.cs
+++ ClientApp/ViewModels/MainViewModel.cs
@@ -1,17 +1,29 @@
-﻿using ClientApp.Utils;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ClientApp.Models;
+using ClientApp.Utils;
+using GalaSoft.MvvmLight.Command;
+using System.Windows.Input;
 
 namespace ClientApp.ViewModels
 {
     class MainViewModel : ObservableObject
     {
-        public string StatusLabelText { get; set; }
+        public Info infoModel { get; set; }
+        public ICommand RetryServerCommand { get; set; }
+        public ICommand RetryVREngineCommand { get; set; }
 
         public MainViewModel()
         {
-            StatusLabelText = "Status: not running";
+            this.infoModel = new Info();
+            this.RetryServerCommand = new RelayCommand(() =>
+            {
+                //try connect server
+                this.infoModel.ConnectedToServer = true;
+            });
+            this.RetryVREngineCommand = new RelayCommand(() =>
+            {
+                //try connect vr-engine
+                this.infoModel.ConnectedToVREngine = true;
+            });
         }
     }
 }
