6159f3f57be439bda13aec348f656782123999aa
shinichi <shinichi@hotmail.nl>
2020-10-07 15:14:39 +0200

    added login logic and text

--- ClientApp/ViewModels/LoginViewModel.cs
+++ ClientApp/ViewModels/LoginViewModel.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
+using System.Windows.Controls;
+using System.Windows.Input;
 using ClientApp.Utils;
+using GalaSoft.MvvmLight.Command;
 
 namespace ClientApp.ViewModels
 {
     class LoginViewModel : ObservableObject
     {
+        public string Username { get; set; }
+        public LoginViewModel()
+        {
+            LoginCommand = new RelayCommand<object>((parameter) =>
+            {
+                Debug.WriteLine($"username {Username} password {((PasswordBox)parameter).Password}");
+                //TODO send username and password to server
+            });
+        }
+
+        public ICommand LoginCommand { get; set; }
     }
 }
