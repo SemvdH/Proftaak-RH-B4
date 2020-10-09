c87c190d14aa57c5d4d0f0b625a170c5220a0521
Sem van der Hoeven <semmer99@gmail.com>
2020-10-09 12:06:24 +0200

    fix dataparser import

--- Server/Communication.cs
+++ Server/Communication.cs
@@ -1,10 +1,10 @@
-﻿using ClientApp.Utils;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO.Pipes;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
+using Client;
 
 namespace Server
 {
